57ca45b11d1b7147c716a7236daba02a0fa5325d
Cory Shaw <coryshaw@gmail.com>
2012-07-11 16:00:50 -1000

    Merged some of Matt's boilerplate code, added events for banner, map, etc

--- js/script.js
+++ js/script.js
@@ -3,13 +3,42 @@ $(document).ready(function(){
 	// Create a popcorn instance by calling Popcorn("#id-of-my-video")
 	var pop = Popcorn("#video");
 
-	// add a footnote at 2 seconds, and remove it at 6 seconds
-    pop.footnote({
-    	start: 0,
-    	end: 12,
-    	text: "Nice here is an overlay",
-    	target: "target"
-    });
+    // Show the banner and pause the movie using the code plugin
+    // http://popcornjs.org/popcorn-docs/plugins/#Code
+    pop.code({
+    	start: 6.5,
+    	onStart: function( options ) {
+            pop.pause();
+            $('#banner').fadeIn('fast');
+        }
+    })
+
+    // Google Map Plugin
+    .googlemap({
+    	start: 10,
+    	end: 15,
+    	type: "SATELITE",
+    	target: "map",
+    	lat: 43.665429,
+    	lng: -79.403323
+    })
+
+    .code({
+    	start: 10,
+    	onStart: function( options ) {
+            $('#map').show();
+        },
+        end: 15,
+        onEnd: function( options ) {
+        	$('#map').hide();
+        }
+    })
+
+    // banner continue link to resume video and hide banner
+    $('a.continue').click(function(){
+    	pop.play();
+    	$('#banner').fadeOut('fast');
+    })
 
     // play the video right away
     pop.play();
