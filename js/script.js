35f030e36e817d48097ef32a3cbe9d1e1564bb5b
Matthew McVickar <matthew@matthewmcvickar.com>
2012-07-17 15:28:25 -0400

    Further development for presentation.

--- js/script.js
+++ js/script.js
@@ -1,22 +1,32 @@
 $(document).ready(function(){
 
+  // Poster.
+  $('#poster').click(function(){
+  	pop.play();
+  	$('#poster .vote_for_you').addClass('gone');
+  	$('#poster').fadeOut('fast');
+  	$('nav').fadeIn('fast');
+  	$('.stars_top').addClass('goodbye');
+  })
+
 	// Create a popcorn instance by calling Popcorn("#id-of-my-video")
 	var pop = Popcorn("#video");
 
     // Show the banner and pause the movie using the code plugin
     // http://popcornjs.org/popcorn-docs/plugins/#Code
     pop.code({
-    	start: 6.5,
+    	start: 8,
     	onStart: function( options ) {
             pop.pause();
-            $('#banner').fadeIn('fast');
+            $('#overlay').fadeIn('fast');
+            $('#input').fadeIn('fast');
         }
     })
 
     // Google Map Plugin
     .googlemap({
-    	start: 8,
-    	end: 15,
+    	start: .25,
+    	end: 50,
     	type: "SATELLITE",
     	target: "map",
     	location: "Honolulu, HI",
@@ -24,39 +34,52 @@ $(document).ready(function(){
     })
     // Show and hide the map
     .code({
-    	start: 8,
+    	start: .25,
     	onStart: function( options ) {
-            $('#map').show();
+          // $('#map').show();
         },
-        end: 15,
+        end: 50,
         onEnd: function( options ) {
-        	$('#map').hide();
+          // $('#map').hide();
         }
     })
 
     // Subtitle plugin
     .subtitle({
-		start: 16,
-		end: 26,
-		target: "subtitle",
-		text: "This is an amazing subtitle"
+  		start: 10,
+  		end: 26,
+  		target: "subtitle",
+  		text: "This is an amazing subtitle"
     })
 
     .subtitle({
-		start: 27,
-		end: 37,
-		target: "subtitle",
-		text: "LIES LIES LIES"
+  		start: 27,
+  		end: 37,
+  		target: "subtitle",
+  		text: "LIES LIES LIES"
     })
 
+    // 'Pinpoint' link.
+    $('li.map').click(function(){
+      $('#input .choices li').addClass('not_chosen')
+      $(this).removeClass('not_chosen').addClass('chosen')
+      $('#input').addClass('clicked');
+    	$('#okay').fadeIn('fast');
+    	$('#no').fadeIn('fast');
+    	$('#map').fadeIn('fast');
+    })
 
-    // banner continue link to resume video and hide banner
-    $('a.continue').click(function(){
+    // 'Continue' link to resume video and hide input banner.
+    $('#okay').click(function(){
     	pop.play();
-    	$('#banner').fadeOut('fast');
+    	$('#overlay').fadeOut('fast');
+    	$('#input').fadeOut('fast');
+    	$('#map').fadeOut('fast');
+    	$('#okay').fadeOut('fast');
+    	$('#no').fadeOut('fast');
     })
 
-    // play the video right away
-    pop.play();
+    // Play the video right away.
+    // pop.play();
 
-}); // end doc ready
+});
