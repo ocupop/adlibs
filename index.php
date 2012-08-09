f1f6f8ffdee082b62191e0d5a8f0cf80e0ee957f
Matthew McVickar <matthew@matthewmcvickar.com>
2012-08-08 18:06:25 -1000

    Changed page title to more accurately reflect the name of the app.

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width">
 
-		<title>Popcorn Admaker</title>
+		<title>PBS NewsHour AdMaker 2012</title>
 
 		<link rel="stylesheet" type="text/css" href="style.css" />
 		<link rel="stylesheet" type="text/css" href="ads.css" />
