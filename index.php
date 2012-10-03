7af35015236b232679efac61f20ee4ce49469e0d
Matthew McVickar <matthew@matthewmcvickar.com>
2012-10-03 13:39:00 -1000

    Added Chartbeat code.

--- index.php
+++ index.php
@@ -5,20 +5,23 @@
   <!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 
   <head>
-    <script type="text/javascript">
+    <script>
+    // Chartbeat.
+    var _sf_startpt=(new Date()).getTime()
 
-      var _gaq = _gaq || [];
-      _gaq.push(['_setAccount', 'UA-1996666-1']);
-      _gaq.push(['_setDomainName', 'www.pbs.org']);
-      _gaq.push(['_trackPageview']);
+    // Google Analytics.
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-1996666-1']);
+    _gaq.push(['_setDomainName', 'www.pbs.org']);
+    _gaq.push(['_trackPageview']);
 
-      (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-      })();
-
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
     </script>
+
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width">
@@ -611,6 +614,32 @@
       </div>
 
     </div>
+
+    <script>
+    // Chartbeat.
+    var _sf_async_config={};
+    /** CONFIGURATION START **/
+    _sf_async_config.uid = 7166;
+    _sf_async_config.domain = "www.pbs.org";
+    /** CONFIGURATION END **/
+    (function(){
+     function loadChartbeat() {
+       window._sf_endpt=(new Date()).getTime();
+       var e = document.createElement("script");
+       e.setAttribute("language", "javascript");
+       e.setAttribute("type", "text/javascript");
+       e.setAttribute("src",
+          (("https:" == document.location.protocol) ?
+            "https://a248.e.akamai.net/chartbeat.download.akamai.com/102508/" :
+            "http://static.chartbeat.com/") +
+          "js/chartbeat.js");
+       document.body.appendChild(e);
+     }
+     var oldonload = window.onload;
+     window.onload = (typeof window.onload != "function") ?
+        loadChartbeat : function() { oldonload(); loadChartbeat(); };
+    })();
+    </script>
   </body>
 
   <script src="js/plugins-min.js"></script>
