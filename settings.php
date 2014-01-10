99fd7b4d42d27ffe4889bc4ee92216cb0967ab4c
unknown <scott.downe@gmail.com>
2014-01-10 03:00:19 -0500

    Ensure we default to production deployment as a fallback case. If it's not local dev or staging, it must be production. So let's optimize for that

--- settings.php
+++ settings.php
@@ -8,15 +8,11 @@ if ($_SERVER["SERVER_NAME"] == 'localhost') {
   $APP_URL = 'http://localhost/adlibs';
   $APP_ENVIRONMENT = 'dev';
   $FB_APP_ID = '461888813829980';
-}
-
-if ($_SERVER["SERVER_NAME"] == 'secure1354.hostgator.com') {
+} else if ($_SERVER["SERVER_NAME"] == 'secure1354.hostgator.com') {
   $APP_URL = 'https://secure1354.hostgator.com/~ocupoppy/pbsadlibs';
   $APP_ENVIRONMENT = 'staging';
   $FB_APP_ID = '158858627586241';
-}
-
-if ($_SERVER["SERVER_NAME"] == 'ec2-107-22-167-218.compute-1.amazonaws.com') {
+} else {
   $APP_URL = 'https://www.pbs.org/newshour/adlibs';
   $APP_ENVIRONMENT = 'production';
   $FB_APP_ID = '331797950244138';
